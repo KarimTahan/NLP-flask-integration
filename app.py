63bbc4a736db35745cbd34ca99c4ec67439b9b44
KarimTahan <taha0025@algonquinlive.com>
2020-03-24 14:34:06 -0400

    * Added seed length box in Index.html * Added handling for seed length in app.py * removed print of character mappings

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('Index.html')
 
 
 @app.route('/param_input', methods=['POST'])
@@ -16,21 +16,20 @@ def get_params():
     id_to_char = []
     char_to_id = {}
 
-    params = {'seed': request.form['seed'], 'model': request.form['model']}
+    params = {'seed': request.form['seed'], 'author': request.form['author'],
+              'seed_length': request.form['seed_length']}
     seed = params['seed'] + " "
-    if params['model'] in 'shakespeare_map.csv':
+    seed_length = int(params['seed_length'])
+    if params['author'] in 'shakespeare_map.csv':
         with open('shakespeare_map.csv') as file:
             reader = csv.reader(file)
             for row in reader:
                 id_to_char.append(row[1])
-            print(id_to_char)
-
             char_to_id = {k: v for v, k in enumerate(id_to_char)}
-            print(char_to_id)
 
     new_model = load_model(len(char_to_id), 'shakespeare_checkpoint')
-
-    prediction = generate_text(new_model, seed, char_to_id, id_to_char, num_to_generate=1000)
+    print('Printing model')
+    prediction = generate_text(new_model, seed, char_to_id, id_to_char, num_to_generate=seed_length)
     return make_response(jsonify(prediction), 200)
 
 
